44307fb7ed9fdc42ccd1589e262b0ad38bb313bb
Adrian7172 <amnchauhan7172@gmail.com>
2022-07-28 11:08:26 +0530

    final

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'www.kindpng.com',
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
